48347f5a8ee3a0a98d07dd97e1362d7989faaab3
Lucas Baierle <lucasbaierle2003@gmail.com>
2025-05-11 16:28:25 -0300

    VSLBlack atualizada

--- src/components/VSLBlack.tsx
+++ src/components/VSLBlack.tsx
@@ -6,26 +6,30 @@ export default function VSLBlack() {
   useEffect(() => {
     const script = document.createElement('script');
     script.id = 'scr_681e206cf93e3486b82673cd';
-    script.src = 'https://player-cdn.com/video/player.js';
+    script.src = 'https://scripts.converteai.net/9e5adb60-2a6e-4137-97bb-0eaa0f5632d0/players/681e206cf93e3486b82673cd/player.js';
     script.async = true;
     document.head.appendChild(script);
 
     return () => {
-      document.head.removeChild(script);
+      const existingScript = document.getElementById('scr_681e206cf93e3486b82673cd');
+      if (existingScript) {
+        existingScript.remove();
+      }
     };
   }, []);
 
   return (
     <div style={{ borderRadius: '16px', overflow: 'hidden' }}>
       <div id="vid_681e206cf93e3486b82673cd" style={{ position: 'relative', width: '100%', padding: '56.25% 0 0' }}>
-        <img 
-          id="thumb_681e206cf93e3486b82673cd" 
-          src="https://player-cdn.com/api/player/video/681e206cf93e3486b82673cd/thumbnail.jpg" 
-          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', borderRadius: '16px' }} 
+        <img
+          id="thumb_681e206cf93e3486b82673cd"
+          src="https://images.converteai.net/9e5adb60-2a6e-4137-97bb-0eaa0f5632d0/players/681e206cf93e3486b82673cd/thumbnail.jpg"
+          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', display: 'block' }}
+          alt="thumbnail"
         />
-        <div 
-          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', cursor: 'pointer', borderRadius: '16px' }} 
-          id="play_681e206cf93e3486b82673cd"
+        <div
+          id="backdrop_681e206cf93e3486b82673cd"
+          style={{ WebkitBackdropFilter: 'blur(5px)', backdropFilter: 'blur(5px)', position: 'absolute', top: 0, height: '100%', width: '100%' }}
         />
       </div>
     </div>
